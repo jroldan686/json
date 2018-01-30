30393836639648fabe2e70249a2d7f9d28651e20
jroldan686 <jroldan686@gmail.com>
2018-01-30 12:45:40 +0100

    Ampliación con Lista de Contactos sin y con Gson y CreacionJson

---
+++ app/src/main/java/acdat/jroldan/json/ListaContactosGsonActivity.java
@@ -0,0 +1,13 @@
+package acdat.jroldan.json;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaContactosGsonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_contactos_gson);
+    }
+}
